3dc7fd172c5a30a07268b627c5a2473d23447390
Doug Kelly <dkelly628@gmail.com>
2015-09-01 16:14:11 -0700

    Initial work

---
+++ Intro2FunctProgScala/build.sbt
@@ -0,0 +1,6 @@
+name := "Intro2FunctProgScala"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ Intro2FunctProgScala/src/main/scala/Week1/ExampleApp.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by Doug on 7/18/15.
+ */
+package Week1
+
+object ExampleApp extends App {
+  println("Hello Scala World!")
+}
